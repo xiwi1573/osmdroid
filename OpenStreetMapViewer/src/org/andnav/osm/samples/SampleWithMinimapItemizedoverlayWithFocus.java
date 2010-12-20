f2b4d6df7d9bdcff1cb4a591d4ef0e50962ca4bd
neilboyd <neilboyd@349629ee-b03d-11dd-8830-69e4bb380a4a>
2010-12-20 12:04:34 +0000

    Some more tidying up. I think the archive provider is working okay now.

--- OpenStreetMapViewer/src/org/andnav/osm/samples/SampleWithMinimapItemizedoverlayWithFocus.java
+++ OpenStreetMapViewer/src/org/andnav/osm/samples/SampleWithMinimapItemizedoverlayWithFocus.java
@@ -5,11 +5,8 @@ import java.util.ArrayList;
 
 import org.andnav.osm.ResourceProxy;
 import org.andnav.osm.ResourceProxyImpl;
-import org.andnav.osm.tileprovider.OpenStreetMapTileProviderDirect;
 import org.andnav.osm.tileprovider.renderer.OpenStreetMapRendererFactory;
 import org.andnav.osm.tileprovider.util.CloudmadeUtil;
-import org.andnav.osm.tileprovider.util.SimpleInvalidationHandler;
-import org.andnav.osm.tileprovider.util.SimpleRegisterReceiver;
 import org.andnav.osm.util.GeoPoint;
 import org.andnav.osm.views.OpenStreetMapView;
 import org.andnav.osm.views.overlay.OpenStreetMapViewItemizedOverlay;
@@ -158,7 +155,4 @@ public class SampleWithMinimapItemizedoverlayWithFocus extends Activity {
 	// Methods
 	// ===========================================================
 
-	// ===========================================================
-	// Inner and Anonymous Classes
-	// ===========================================================
-}
+	// =========
